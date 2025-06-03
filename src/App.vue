ef7bc5a5de4755c9174e15e7c9b02a69a35c0899
lmao <lmao.mr@foxmail.com>
2025-06-03 09:20:16 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <RouterView />
+  </header>
+</template>
+
+<style scoped></style>
